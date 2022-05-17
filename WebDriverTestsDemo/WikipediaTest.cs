728a4c24b70cb0744adc25759734733985113444
Gasha <Gasha@LAP-GASHA>
2022-05-17 19:27:10 +0300

    Add project files.

---
+++ WebDriverTestsDemo/WikipediaTest.cs
@@ -0,0 +1,25 @@
+﻿
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+// Create new ChromBraser instance
+var driver = new ChromeDriver();
+
+// Navigate ti Wikipedia
+driver.Url = "https://wikipedoa.org";
+
+// Locate Search field by ID
+var searchField = driver.FindElement(By.Id("searchInput"));
+
+// Clik on element
+searchField.Click();
+
+// fill QA and press ENTER keybord button
+searchField.SendKeys("QA");
+searchField.SendKeys(Keys.Enter);
+
+System.Console.WriteLine("TITEL AFTER SEARCH: " + driver.Title);
+
+//Close browser
+driver.Quit();
